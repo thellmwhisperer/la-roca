662b4a372ec0e53c8d80e1171e765e96e2c5f275
Javier Mellado <sol@javiermellado.com>
2026-08-14 04:04:20 +0100

    Revert "feat(vector): add optional local semantic search under roca vector (#103)" (#105)
    This reverts commit 0b48525b6b6a2a1234489e4f5e9ceeae7c5c0217.

--- internal/vector/detach_unix.go
+++
@@ -1,7 +0,0 @@
-//go:build !windows
-
-package vector
-
-import "syscall"
-
-var detachedProcessAttributes = &syscall.SysProcAttr{Setsid: true}
--- internal/vector/detach_windows.go
+++
@@ -1,7 +0,0 @@
-//go:build windows
-
-package vector
-
-import "syscall"
-
-var detachedProcessAttributes = &syscall.SysProcAttr{CreationFlags: 0x00000008 | 0x00000200}
